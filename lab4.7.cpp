f28d6f43518b32081cf2d47e28a2727d833e1d09
Yaroslava <72552637+Yyaroslava@users.noreply.github.com>
2020-11-03 17:40:17 +0200

    Add files via upload

--- lab4.7.cpp
+++ lab4.7.cpp
@@ -1,45 +1,47 @@
-#include <iostream> 
-#include <iomanip> 
-#include <cmath> 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
-    int n = 0;
+int main() {
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+	int n = 0;
 
-    cout << "xp = ";
-    cin >> xp;
-    cout << "xk = ";
-    cin >> xk;
-    cout << "dx = ";
-    cin >> dx;
-    cout << "eps = ";
-    cin >> eps;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "     |"
+		<< setw(10) << "Arth(x)" << "   |"
+		<< setw(7) << "S" << "      |"
+		<< setw(5) << "n" << "   |" << endl;
+	cout << "-------------------------------------------------" << endl;
 
-    cout << fixed;
+	x = xp;
 
-    cout << "-------------------------------------------------" << endl;
-    cout << "|" << setw(5) << "x" << "     |" << setw(10) << "Arth(x)" << "   |" << setw(7) << "S" << "      |" << setw(5) << "n" << "   |" << endl;
-    cout << "-------------------------------------------------" << endl;
-    x = xp;
-    while (x <= xk)
-    {
-        n = 0;                // вираз залежить від умови завдання варіанту         
-        a = x;                // вираз залежить від умови завдання варіанту         
-        S = a;
-        do
-        {
-            n++;
-            R = (2.*n*pow(x,2)-pow(x,2))/(2.*n+1);          // вираз залежить від умови завдання варіанту     
-            a *= R;
-            S += a;
-        } while (abs(a) >= eps);
+	while (x <= xk)
+	{
+		n = 0;
+		a = x;
+		S = a;
+		do {
+			n++;
+			R = x*x*((2.*n-1)/(2.*n+1));
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		cout << "|" << setw(7) << setprecision(2) << x << "   |"
+			<< setw(10) << setprecision(5) << 0.5 * ((log(1. + x) - log(1. - x))) << "   |"
+			<< setw(10) << setprecision(5) << S << "   |"
+			<< setw(5) << setprecision(0) << n << "   |" << endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
 
-        cout << "|" << setw(7) << setprecision(2) << x << "   |" << setw(10) << setprecision(5) << exp(x) << "   |" << setw(10) << setprecision(5) << S << "   |" << setw(5) << n << "   |" << endl;
-        x += dx;
-    }
-    cout << "-------------------------------------------------" << endl;
-    return 0;
+	cin.get();
+	system("pause");
+	return 0;
 }
